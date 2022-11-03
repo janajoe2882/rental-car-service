da7a144bc64815c6e30bbef71195b02fe225925d
janajoe2882 <wyswid@naver.com>
2022-11-03 12:51:03 +0900

    chore: add meta name

--- src/components/molecules/MetaBox.js
+++ src/components/molecules/MetaBox.js
@@ -11,6 +11,7 @@ export default function MetaBox(props) {
       <meta property='title' content={name} />
       <meta property='og:title' content={name} />
       <meta property='description' content={brand} />
+      <meta name='description' content={brand} />
       <meta property='og:description' content={brand} />
       <meta property='og:url' content={`${location.pathname}`} />
       <meta property='og:image' content={img} />
