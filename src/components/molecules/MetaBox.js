91610679d10a7015ad538f0f8887b28496587d1e
janajoe2882 <wyswid@naver.com>
2022-11-03 16:18:54 +0900

    chore: add data-react-helmet value

--- src/components/molecules/MetaBox.js
+++ src/components/molecules/MetaBox.js
@@ -6,18 +6,54 @@ export default function MetaBox(props) {
   return (
     <Helmet>
       <title>{name}</title>
-      <meta data-rh='true' property='title' content={name} />
-      <meta data-rh='true' property='og:title' content={name} />
       <meta
         data-rh='true'
+        data-react-helmet='true'
+        property='title'
+        content={name}
+      />
+      <meta
+        data-rh='true'
+        data-react-helmet='true'
+        property='og:title'
+        content={name}
+      />
+      <meta
+        data-rh='true'
+        data-react-helmet='true'
         property='og:url'
         content={`https://steady-unicorn-2a5384.netlify.app/${carId}`}
       />
-      <meta data-rh='true' property='description' content={brand} />
-      <meta data-rh='true' property='og:description' content={brand} />
-      <meta data-rh='true' property='price' content={`${amount}원`} />
-      <meta data-rh='true' property='og:price' content={`${amount}원`} />
-      <meta data-rh='true' property='og:image' content={img} />
+      <meta
+        data-rh='true'
+        data-react-helmet='true'
+        property='description'
+        content={brand}
+      />
+      <meta
+        data-rh='true'
+        data-react-helmet='true'
+        property='og:description'
+        content={brand}
+      />
+      <meta
+        data-rh='true'
+        data-react-helmet='true'
+        property='price'
+        content={`${amount}원`}
+      />
+      <meta
+        data-rh='true'
+        data-react-helmet='true'
+        property='og:price'
+        content={`${amount}원`}
+      />
+      <meta
+        data-rh='true'
+        data-react-helmet='true'
+        property='og:image'
+        content={img}
+      />
     </Helmet>
   );
 }
