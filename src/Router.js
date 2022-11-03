5f49901632025faafe2d5bb50778535f9c939a31
janajoe2882 <wyswid@naver.com>
2022-11-03 13:17:45 +0900

    fix: add data-rh value to meta tag

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 import CarList from './pages/CarList';
 import CarDescription from './pages/CarDescription';
 import Error from './pages/Error';
@@ -8,6 +9,9 @@ import Layout from './styles/Layout';
 function Router() {
   return (
     <BrowserRouter>
+      <Helmet>
+        <title>알티 모빌리티</title>
+      </Helmet>
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<CarList />} />
--- src/components/molecules/MetaBox.js
+++ src/components/molecules/MetaBox.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import { useLocation } from 'react-router-dom';
 
 export default function MetaBox(props) {
@@ -8,13 +8,13 @@ export default function MetaBox(props) {
   return (
     <Helmet>
       <title>{name}</title>
-      <meta property='title' content={name} />
-      <meta property='og:title' content={name} />
-      <meta property='description' content={brand} />
-      <meta name='description' content={brand} />
-      <meta property='og:description' content={brand} />
-      <meta property='og:url' content={`${location.pathname}`} />
-      <meta property='og:image' content={img} />
+      <meta data-rh='true' property='title' content={name} />
+      <meta data-rh='true' property='og:title' content={name} />
+      <meta data-rh='true' property='description' content={brand} />
+      <meta data-rh='true' name='description' content={brand} />
+      <meta data-rh='true' property='og:description' content={brand} />
+      <meta data-rh='true' property='og:url' content={`${location.pathname}`} />
+      <meta data-rh='true' property='og:image' content={img} />
     </Helmet>
   );
 }
